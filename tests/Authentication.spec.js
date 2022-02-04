ed84f5e5e3f6eab853d2b2dff1180f3e32bf475a
Aurélien Lair <aurelien.lair@musement.com>
2022-02-04 23:48:24 +0100

    feat: add first Playright test

---
+++ tests/Authentication.spec.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+require('playwright');
+
+describe('Signup Test Suite', () => {
+  it('Authentication', async () => {
+    await page.goto('https://localcoding.us/user/login', {
+      waitUntil: 'networkidle0',
+    });
+  });
+
+  it('Log in successfully with valid credentials', async () => {
+    await page.waitForSelector('input[id="normal_login_email"]', {
+      visible: true,
+    });
+    await page.type('input[id="normal_login_email"]', 'example@mail.com');
+    await page.waitForSelector('input[id="normal_login_password"]', {
+      visible: true,
+    });
+    await page.type('input[id="normal_login_password"]', 'supersecretpassword');
+    await page.waitForSelector('button[type="submit"]', {
+      visible: true,
+    });
+    await page.click('button[type="submit"]');
+  });
+});
